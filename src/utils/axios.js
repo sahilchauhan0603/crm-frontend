0bb2e26b126f6bd90ee20c12434e019be7e7558f
sahilchauhan0603 <sahilchauhan0603@gmail.com>
2025-06-02 14:29:43 +0530

    backend url specified

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:5000', // Replace with your backend URL
+  // baseURL: 'http://localhost:5000', 
+  baseURL: 'https://crm-backend-v6pq.onrender.com', 
   headers: {
     'Content-Type': 'application/json',
   },
